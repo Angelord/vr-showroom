3bce5faf3fb8cb060377113aa876b05f5244c6ee
Angel Georgiev <angel.georgiev@diadraw.com>
2019-12-18 14:25:45 +0200

    Add show and hide effects to object info gui.

--- Assets/Scripts/UserInterface/ObjectInformationGUI.cs
+++ Assets/Scripts/UserInterface/ObjectInformationGUI.cs
@@ -1,35 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Showroom.Utility;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Showroom.UserInterface {
+	[RequireComponent(typeof(Animator))]
 	public class ObjectInformationGUI : MonoBehaviour {
 
+		private static readonly int AnimPropHidden = Animator.StringToHash("Hidden");
+		
 		[SerializeField] private Text _nameText;
 		[SerializeField] private Text _dimensionsText;
 		[SerializeField] private Text _materialText;
 		[SerializeField] private Image _separatorLine;
-		
+
+		private Animator _animator;
 		private ObjectInformation _current;
 
 		private void Start() {
+			_animator = GetComponent<Animator>();
 			Hide();
 		}
 
 		public void Show(ObjectInformation information) {
 			if (_current != null) {
 				_current.OnMaterialChange -= OnMaterialChange;
+				_animator.SetBool(AnimPropHidden, true);
 			}
 
 			_current = information;
 			_current.OnMaterialChange += OnMaterialChange;
 
-			_nameText.text = _current.Name;
-			_dimensionsText.text = _current.Dimensions;
-			_materialText.text = _current.Material;
-			_separatorLine.gameObject.SetActive(true);
-			//TODO : Play show animation
+			CustomCoroutine.WaitOneFrameThenExecute(() => {
+				_animator.SetBool(AnimPropHidden, false);
+			});
 		}
 
 		public void Hide() {
@@ -37,12 +42,14 @@ namespace Showroom.UserInterface {
 				_current.OnMaterialChange -= OnMaterialChange;
 				_current = null;
 			}
+			
+			_animator.SetBool(AnimPropHidden, true);
+		}
 
-			_nameText.text = "";
-			_dimensionsText.text = "";
-			_materialText.text = "";
-			_separatorLine.gameObject.SetActive(false);
-			//TODO : Play hide animation
+		public void OnShowAnimationStarted() {
+			_nameText.text = _current.Name;
+			_dimensionsText.text = _current.Dimensions;
+			_materialText.text = _current.Material;
 		}
 
 		private void OnMaterialChange() {
