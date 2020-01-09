10a082e5d9a68a6ca9e9ce96421d64f7468adf83
Angel Georgiev <angel.georgiev@diadraw.com>
2020-01-09 16:44:22 +0200

    Add inertia to free navigation.

--- Assets/Scripts/Navigation/FreeNavigation.cs
+++ Assets/Scripts/Navigation/FreeNavigation.cs
@@ -6,8 +6,10 @@ namespace Showroom.Navigation {
         
         [SerializeField] private float _yOffset = 1.6f;
         [SerializeField] private float _rotateSensitivity = 2.0f;
+        [SerializeField] [Range(0.0f, 1.0f)] private float _inertiaMultiplier = 0.94f;
         private bool _rotating;
         private bool _moving;
+        private Vector3 _rotInertia = Vector3.zero;
 
         public override bool Locked => _rotating || _moving;
 
@@ -24,22 +26,27 @@ namespace Showroom.Navigation {
 
         private void Update() {
             
-            if(Input.GetMouseButton(0)) {
-                Vector3 offset = new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0.0f);
-                offset *= _rotateSensitivity;
-            
-                transform.Rotate(offset);
+            _rotating = false;
 
-                Vector3 rotation = transform.rotation.eulerAngles;
-                rotation.z = 0.0f;
-                transform.rotation = Quaternion.Euler(rotation);
-
-                if (offset.sqrMagnitude > 0.1f) {
-                    _rotating = true;
-                }
+            Vector3 input = Vector3.zero;
+            if (Input.GetMouseButton(0)) {
+                input = new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0.0f);
             }
-            else {
-                _rotating = false;
+            
+            Vector3 offset = input + _rotInertia;
+            offset *= _rotateSensitivity;
+
+            _rotInertia += input / 2;
+            _rotInertia *= _inertiaMultiplier;
+        
+            transform.Rotate(offset);
+
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.z = 0.0f;
+            transform.rotation = Quaternion.Euler(rotation);
+
+            if (offset.sqrMagnitude > 0.1f) {
+                _rotating = true;
             }
         }
     }
