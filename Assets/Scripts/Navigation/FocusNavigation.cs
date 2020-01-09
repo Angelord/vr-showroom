28afe2f85115f4dcf80b36f398507be2b1848549
Angel Georgiev <angel.georgiev@diadraw.com>
2020-01-09 16:37:27 +0200

    Add inertia to focus navigation.

--- Assets/Scripts/Navigation/FocusNavigation.cs
+++ Assets/Scripts/Navigation/FocusNavigation.cs
@@ -12,11 +12,14 @@ namespace Showroom.Navigation {
         [SerializeField] private float _rotateSensitivity = 2.0f;
         [SerializeField] private float _maxZoom = 1.5f;
         [SerializeField] private float _zoomSpeed = 2.0f;
+        [SerializeField] [Range(0.0f, 1.0f)] private float _inertiaMultiplier = 0.9f;
         private Camera _camera;
         private InteractableObject _target;
         private Transform _focusPoint;
         private bool _focusing;
         private bool _rotating;
+        private float _zoomIntertia = 0.0f;
+        private Vector3 _rotInertia = Vector2.zero;
         
         public override bool Locked => _rotating || _focusing;
 
@@ -77,32 +80,43 @@ namespace Showroom.Navigation {
 
         private void HandleZoom() {
             float mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+            mouseWheel = Mathf.Clamp(_zoomIntertia + mouseWheel, -1.0f, 1.0f);
+            
             float cameraZ = _camera.transform.localPosition.z;
-            if ((mouseWheel > 0.01f && cameraZ < _maxZoom) || (mouseWheel < -0.01f && cameraZ > 0.0f)) {
+            if ((mouseWheel > 0.0001f && cameraZ < _maxZoom) || (mouseWheel < -0.0001f && cameraZ > 0.0f)) {
                 _camera.transform.Translate(Time.deltaTime * _zoomSpeed * mouseWheel * Vector3.forward);
             }
+            
+            _zoomIntertia *= _inertiaMultiplier;
+            _zoomIntertia += mouseWheel;
+            _zoomIntertia /= 2;
         }
 
         private void HandleRotation() {
+            _rotating = false;
+
+            Vector3 input = Vector3.zero;
             if (Input.GetMouseButton(0)) {
-                Vector3 offset = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0.0f);
-                offset *= _rotateSensitivity;
-                
-                _focusPoint.Rotate(Vector3.up, offset.x, Space.World);
-
-                float camAngle = Vector3.Angle(_camera.transform.forward, Vector3.up);
-                if ((offset.y > 0.0f && camAngle > MinRotationZ) || (offset.y < 0.0f && camAngle < MaxRotationZ)) {
-                    Vector3 lookDir = _focusPoint.position - transform.position;
-                    Vector3 camRight = Vector3.Cross(lookDir, Vector3.up);
-                    _focusPoint.Rotate(camRight, offset.y, Space.World);
-                }
-
-                if (offset.sqrMagnitude > 0.1f) {
-                    _rotating = true;
-                }
+                input = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0.0f);
             }
-            else {
-                _rotating = false;
+
+            Vector3 offset = input + _rotInertia;
+            offset *= _rotateSensitivity;
+
+            _rotInertia += input / 2;
+            _rotInertia *= _inertiaMultiplier;
+            
+            _focusPoint.Rotate(Vector3.up, offset.x, Space.World);
+
+            float camAngle = Vector3.Angle(_camera.transform.forward, Vector3.up);
+            if ((offset.y > 0.0f && camAngle > MinRotationZ) || (offset.y < 0.0f && camAngle < MaxRotationZ)) {
+                Vector3 lookDir = _focusPoint.position - transform.position;
+                Vector3 camRight = Vector3.Cross(lookDir, Vector3.up);
+                _focusPoint.Rotate(camRight, offset.y, Space.World);
+            }
+
+            if (offset.sqrMagnitude > 0.1f) {
+                _rotating = true;
             }
         }
     }
